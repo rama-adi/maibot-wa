8c72c5a994914a1ee24e923eba715036a71b0b14
ramaadi <17980909+rama-adi@users.noreply.github.com>
2025-06-08 02:18:16 +0700

    fix drizzle

--- src/database/drizzle.ts
+++ src/database/drizzle.ts
@@ -3,10 +3,10 @@ import { Database } from 'bun:sqlite';
 import * as musicSchema from "@/database/schemas/music-schema";
 
 // Music database
-const musicSqlite = new Database("data/sheet_score.db");
+const musicSqlite = new Database("../../data/sheet_score.db");
 export const musicDatabase = drizzle(musicSqlite, { schema: musicSchema });
 
 
 import * as configSchema from "@/database/schemas/config-schema";
-const configSqlite = new Database("data/bot_config.db");
+const configSqlite = new Database("../../data/bot_config.db");
 export const configDatabase = drizzle(configSqlite, { schema: configSchema });
