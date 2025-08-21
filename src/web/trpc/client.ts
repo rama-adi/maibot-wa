c3eac9bceadaed4dc1761fabede84a5b22d4643e
ramaadi <17980909+rama-adi@users.noreply.github.com>
2025-08-22 00:58:35 +0800

    update client host

--- src/web/trpc/client.ts
+++ src/web/trpc/client.ts
@@ -5,7 +5,7 @@ import type { AppRouter } from "@/web/trpc/index";
 export const queryClient = new QueryClient();
 
 const trpcClient = createTRPCClient<AppRouter>({
-    links: [httpBatchLink({ url: 'http://localhost:3000/trpc' })],
+    links: [httpBatchLink({ url: process.env.BUN_PUBLIC_HOST || "" })],
 });
 
 export const trpc = createTRPCOptionsProxy<AppRouter>({
